6be461b2ab0c32810f46523905a8420d3472b6fa
vieroBruno <viero.g.bruno@gmail.com>
2025-11-15 17:05:30 -0300

    Back-end da aplicação

---
+++ back-end/src/main/java/com/example/demo/controller/WeatherController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/api/weather")
+public class WeatherController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Value("${openweathermap.api.key}")
+    private String apiKey;
+
+    @GetMapping("/{city}")
+    public Object getWeatherData(@PathVariable String city) {
+
+        String url = "https://api.openweathermap.org/data/2.5/weather?q=" + city
+                + "&appid=" + apiKey
+                + "&units=metric&lang=pt_br";
+
+        return restTemplate.getForObject(url, Object.class);
+    }
+}
