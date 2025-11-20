2c038a6774c832e9e9a42d328e38f2b2e99aced4
Bruno Gabriel Viero <bviero@softexpert.com>
2025-11-20 19:21:34 -0300

    Adição de rota e remoção de arquivo

--- back-end/src/main/java/com/example/demo/controller/WeatherController.java
+++ back-end/src/main/java/com/example/demo/controller/WeatherController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -24,6 +25,16 @@ public class WeatherController {
         String url = "https://api.openweathermap.org/data/2.5/weather?q=" + city
                 + "&appid=" + apiKey
                 + "&units=metric&lang=pt_br";
+
+        return restTemplate.getForObject(url, Object.class);
+    }
+
+    @GetMapping("/coordinates")
+    public Object geWeatherByLatAndLon(@RequestParam double lat,@RequestParam double lon) {
+        String url = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat
+            + "&lon=" + lon
+            + "&appid=" + apiKey
+            + "&units=metric&lang=pt_br";
 
         return restTemplate.getForObject(url, Object.class);
     }
